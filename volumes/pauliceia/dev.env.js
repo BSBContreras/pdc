b3b90f119b2c3a8644296e62ab7b1b4391bc3d17
Rodrigo Mariano <rodmariano13@gmail.com>
2022-02-09 14:26:33 -0300

    fix environment variables

--- volumes/pauliceia/dev.env.js
+++ volumes/pauliceia/dev.env.js
@@ -5,9 +5,10 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   urlVGI: '"http://localhost:8080/api/vgimws"',
+  // urlVGI: '"http://localhost:3001"',
   // urlVGI: '"http://www.pauliceia.dpi.inpe.br/api/vgi"',
-  urlGeocoding: '"http://localhost:8080/api/geocoding"',
-  // urlGeocoding: '"http://www.pauliceia.dpi.inpe.br/api/geocoding"',
+  // urlGeocoding: '"http://localhost:8080/api/geocoding"',
+  urlGeocoding: '"http://www.pauliceia.dpi.inpe.br/api/geocoding"',
   urlGeoserverPauliceia: '"http://localhost:8080/geoserver/pauliceia"',
   // urlGeoserverPauliceia: '"http://www.pauliceia.dpi.inpe.br/geoserver/pauliceia"',
   urlGeoserveOther: '"http://localhost:8080/geoserver/other"',
